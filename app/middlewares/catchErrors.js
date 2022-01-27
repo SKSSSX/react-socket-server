a32501daae27c0d295a1a9f2b18e531f518bdf43
SKSSSX <1270256946@qq.com>
2022-01-27 09:29:10 +0800

    feat: first commit

---
+++ app/middlewares/catchErrors.js
@@ -0,0 +1,12 @@
+const catchError = async(ctx, next) => {
+  try {
+    await next();
+  } catch (error) {
+    if (error.errorCode) {
+      console.log("捕获到异常");
+      return ctx.body = error.msg;
+    }
+  }
+}
+
+module.exports = catchError;
